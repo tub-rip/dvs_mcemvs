b9ed2f2021e4e2811ad11e687c3e786e8d856e2e
Suman Ghosh <35840258+ghoshsuman@users.noreply.github.com>
2022-10-27 20:44:12 +0200

    Add files via upload

--- mapper_emvs_stereo/scripts/mocap_txt2bag.py
+++ mapper_emvs_stereo/scripts/mocap_txt2bag.py
@@ -9,18 +9,15 @@ from tqdm import tqdm
 import argparse
 
 parser = argparse.ArgumentParser()
-# parser.add_argument("bag", help="ROS bag file to extract")
 parser.add_argument("--path_prefix", help="Folder that contains input pose")
-# parser.add_argument("--pose_topic", default="/pose", help="Pose topic")
 args = parser.parse_args()
 path_prefix = args.path_prefix
 # path_prefix = '/mnt/HD4TB/data/tum-vie/slide-vi_gt_data/'
 
-pose_fname = path_prefix + 'mocap_data.txt'
+pose_fname = os.path.join(path_prefix, 'mocap_data.txt')
 pos = np.loadtxt(pose_fname, delimiter=' ', skiprows=1)
 
 bag_file_name = path_prefix + 'pose'
-# topic = args.pose_topic
 topic = '/pose'
 
 with rosbag.Bag('{}.bag'.format(bag_file_name), 'w') as bag:
